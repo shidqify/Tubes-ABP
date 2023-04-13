7389d43a59fdd862a3078c29cd89abc0b325c069
Shidqi Yusri <shidqifadhlu@gmail.com>
2023-04-14 03:16:37 +0700

    fix: add token generator

--- bin/modules/user/userService.js
+++ bin/modules/user/userService.js
@@ -5,6 +5,12 @@ const mongoDb = new MongoDb(config.get('/mongoDbUrl'));
 const wrapper = require('../../helpers/utils/wrapper');
 const { NotFoundError, ConflictError } = require('../../helpers/error');
 const md5 = require('md5');
+const jwt = require('jsonwebtoken');
+
+const generateToken = async (id) => {
+    const token = jwt.sign({ id }, config.get('/authentication'));
+    return token;
+  };
 
 module.exports.login = async (username, password) => {
     mongoDb.setCollection('user');
@@ -14,9 +20,14 @@ module.exports.login = async (username, password) => {
     });
     if (recordSet.err) {
         // return wrapper.error(new NotFoundError('Can not find User'));
-        throw new NotFoundError('Can not find user');
+        throw new NotFoundError('Wrong Username / Password');
     }
-    return recordSet;
+    const userData = recordSet.data;
+    const result = {
+        userData,
+        token : await generateToken(recordSet._id)
+    }
+    return result;
 }
 
 module.exports.register = async (username, password, fullname, org) => {
